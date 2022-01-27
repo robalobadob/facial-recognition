6b2d8433f39405d742703c39c9e22bd785443f35
Rob Hoover <rtrhoover@gmail.com>
2022-01-27 15:06:29 -0800

    added logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import facelogo from './facelogo.png';
+import './Logo.css';
 
-const Navigation = () => {
+const Logo = () => {
     return (
         <div className='ma4 mt0'>
-
+            <Tilt>
+                <div style={{ height: '100px', backgroundColor: 'darkgreen' }}>
+                    <h1 className="pa3"><img style={{paddingTop: '5px'}} alt='logo' src={facelogo}/></h1>
+                </div>
+            </Tilt>
         </div>
     );
 }
 
-export default Navigation;
+export default Logo;
