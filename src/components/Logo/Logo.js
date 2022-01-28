1eeca75870e3ca7dc4c9ac4dee723e1414f5a283
Rob Hoover <rtrhoover@gmail.com>
2022-01-28 12:37:01 -0800

    customized logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,10 +5,10 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0'>
+        <div className='ma4 mt0 flex'>
             <Tilt>
-                <div style={{ height: '100px', backgroundColor: 'darkgreen' }}>
-                    <h1 className="pa3"><img style={{paddingTop: '5px'}} alt='logo' src={facelogo}/></h1>
+                <div className='ba grow shadow-5' style={{ height: '100px', width: '100px', background: 'linear-gradient(89deg, #00afe4 0%, #fce423 100%' }}>
+                    <h1><img style={{padding: '5px'}} alt='logo' src={facelogo}/></h1>
                 </div>
             </Tilt>
         </div>
