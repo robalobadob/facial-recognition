34b9022ffaead38b7e485530a493e766ad448ae7
Rob Hoover <rtrhoover@gmail.com>
2022-01-28 01:19:07 -0800

    updating components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Navigation />
-    <Logo />
-     {/*<ImageLinkForm />
-     <FaceRecognition /> */}
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+     {/*<FaceRecognition /> */}
     </div>
   );
 }
