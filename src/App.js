98314e4901e85a0d594fbb3906d50ab8a116cf1d
Rob Hoover <rtrhoover@gmail.com>
2022-01-28 14:02:37 -0800

    added TSParticles and changed link CSS pattern

--- src/App.js
+++ src/App.js
@@ -1,13 +1,98 @@
 import React, { Component } from 'react';
+import Particles from "react-tsparticles";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
+const particlesOptions = {
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 4,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.6,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        random: true,
+        value: 3,
+      },
+    },
+    detectRetina: true,
+  }  
+
 function App() {
+  const particlesInit = (main) => {
+  };
+  const particlesLoaded = (container) => {
+  };
   return (
     <div className="App">
+      <Particles 
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particlesOptions}
+        className='particles'
+      />
       <Navigation />
       <Logo />
       <Rank />
