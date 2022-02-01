8174f9cebeda60a46721787540c7439be3aa658d
Rob Hoover <rtrhoover@gmail.com>
2022-02-01 15:15:15 -0800

    developing face detection box

--- src/App.js
+++ src/App.js
@@ -90,8 +90,13 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = () => {
+
   }
 
   onInputChange = (event) => {
@@ -104,14 +109,8 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then( 
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err));
   }
 
   render() {
