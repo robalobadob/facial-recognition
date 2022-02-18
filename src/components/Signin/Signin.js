86301f995a2000ccf938f56912b28468f4e4e899
Rob Hoover <rtrhoover@gmail.com>
2022-02-18 13:11:45 -0800

    connected signin, rank to database, and updated image submission code for clarity

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 class Signin extends React.Component {
     constructor() {
@@ -26,8 +26,9 @@ class Signin extends React.Component {
             })
         })
             .then(response => response.json())
-            .then(data => {
-                if (data === 'success') {
+            .then(user => {
+                if (user.id) {
+                    this.props.loadUser(user);
                     this.props.onRouteChange('home');
                 }
             })
