3dd753383562ab0b0217a2c55f8c7a8f3e0c8e52
Rob Hoover <rtrhoover@gmail.com>
2022-04-06 00:53:48 -0700

    add cors

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,10 @@ class Register extends React.Component {
     onSubmitSignIn = () => {
         fetch('https://stormy-scrubland-95745.herokuapp.com/register', {
             method: 'post',
-            headers: {'Content-Type': 'application/json'},
+            headers: {
+                'Content-Type': 'application/json', 
+                'Access-Control-Allow-Origin': '*'
+            },
             body: JSON.stringify({
                 email: this.state.email,
                 password: this.state.password,
